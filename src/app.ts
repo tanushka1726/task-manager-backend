5c380ce076a6be9f01d69acd821a56a71487644b
tanushka1726 <tanushka1726@gmail.com>
2026-02-20 21:01:26 +0530

    fix path

--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ app.use(
 );
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/tasks", taskRoutes);
+app.use("/auth", authRoutes);
+app.use("/tasks", taskRoutes);
 
 export default app;
